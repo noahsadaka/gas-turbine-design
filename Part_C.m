7e2fcf214b47dafbbe10e4a09db02862d2c48999
noahsadaka <noahsadaka@gmail.com>
2019-02-22 13:42:09 -0500

    Added shock and acceleration losses

--- Part_C.m
+++ Part_C.m
@@ -19,7 +19,7 @@ clc;clear all;
 
 % Cycle Analysis Variables
 
-% eta_i = 0.87; % desired efficiency
+% eta_i = 0.85; % desired efficiency
 
 error = 0; % flag for errors
 
@@ -44,16 +44,16 @@ gamma = 1.333; % Gas Gamma
 Rg = 287; % Gas Constant
 
 % Assumptions
-alpha_3 = 30; % Blade exit swirl angle [deg] Range: -5 to 30
+alpha_3 = 10; % Blade exit swirl angle [deg] Range: -5 to 30
 M_3 = 0.45; % Blade exit mach number. Range: 0.3-0.45
-R = 0.45; % Reaction at the meanline.
+R = 0.4; % Reaction at the meanline.
 max_U_h = 1100*0.3048; % Blade speed at meanline [m/s]
 inc_1 = 0; % Vane Incidence [deg]
 dev_2 = 0; % Vane Deviation [deg]
-inc_2 = 0; % Blade Incidence [deg]
+inc_2 = 0; % Blade Incidence [deg]vane_axial_chord
 dev_3 = 0; % Blade Deviation [deg]
-zweif_vane = 0.8;
-zweif_blade = 0.85;
+zweif_vane = 0.7;
+zweif_blade = 0.8;
 
 % Given Vane Variables
 AR_v = 0.7; % Vane Aspect Ratio
@@ -95,9 +95,8 @@ A_2 = A_3; % Assumption: rotor area is constant
 T_2 = R * (T_1 - T_3) + T_3; % Temperature at station 2 [K]
 V_2 = sqrt(2*Cp*(To_2 - T_2)); % Absolute Velocity [m/s]
 M_2 = V_2/sqrt(T_2 * gamma * Rg); % Mach
-% need to determine pressure another way
-P_2 = R * (P_1 - P_3) + P_3;
-Po_2 = P_2*(To_2/T_2)^(gamma/(gamma-1));
+Po_2 = Po_1 * (To_2/To_1)^(gamma/(gamma-1)); % Total pressure [Pa]
+P_2 = Po_2 * (1+0.5 * (gamma-1)*M_2^2)^(-gamma/(gamma-1)); % Static pressure [Pa]
 rho_2 = P_2/(Rg * T_2); % Density [kg/m3]
 Va_2 = m_2 / (rho_2 * A_2); % Axial Velocity [m/s]
 Vu_2 = sqrt(V_2^2 - Va_2^2); % Swirl Velocity [m/s]
@@ -108,7 +107,6 @@ U = Ws / (Vu_2 + Vu_3); % Blade Velocity [m/s]
 Vru_2 = Vu_2 - U; % Relative swirl velocity [m/s]
 Vr_2 = sqrt(Va_2^2 + Vru_2^2); % Relative velocity [m/s]
 alpha_r_2 = atand(Vru_2 / Va_2); % Relative swirl angle [deg]
-%T_r_2 = To_2 - (Vr_2^2)/(2*Cp); % Relative temperature [K]
 To_r_2 = T_2 + Vr_2^2/(2*Cp);
 Po_r_2 = P_2 * (T_2 / To_r_2)^(-gamma / (gamma - 1)); % Relative Total Pressure[Pa]
 M_r_2 = Vr_2/sqrt(gamma*Rg*T_2); % Relative Mach
@@ -117,9 +115,6 @@ M_r_2 = Vr_2/sqrt(gamma*Rg*T_2); % Relative Mach
 Vru_3 = U + Vu_3; % Relative swirl velocity [m/s]
 Vr_3 = sqrt(Vru_3^2 + Va_3^2); % Relative Velocity [m/s]
 alpha_r_3 = atand(Vr_3/Va_3); % Relative swirl angle [deg]
-% T_r_3 = To_3 - (Vr_3^2)/(2*Cp); % Relative temperature [K]
-% Po_r_3 = P_3 * (T_r_3 / To_3)^(gamma / (gamma - 1)); % Relative Total Pressure [Pa]
-% M_r_3 = Vr_3/sqrt(gamma*Rg*T_r_3); % Relative Mach
 To_r_3 = T_3 + Vr_3^2/(2*Cp);
 Po_r_3 = P_3 * (T_3 / To_r_3)^(-gamma / (gamma - 1)); % Relative Total Pressure[Pa]
 M_r_3 = Vr_3/sqrt(gamma*Rg*T_3); % Relative Mach
@@ -215,6 +210,8 @@ Vu_h_1 = (r_m_1/r_h_1) * Vu_1; % hub swirl vel [m/s]
 Va_h_1 = Va_1; % hub axial vel [m/s]
 V_h_1 = sqrt(Va_h_1^2 + Vu_h_1^2); % hub velocity [m/s]
 alpha_h_1 = atand(Vu_h_1/Va_h_1); % Swirl angle [deg]
+T_h_1 = To_1 - V_h_1^2/(2*Cp);
+M_h_1 = V_h_1 / sqrt(gamma*Rg*T_h_1);
 
 % Station 2
 Va_h_2 = Va_2;
@@ -224,7 +221,10 @@ alpha_h_2 = atand(Vu_h_2/Va_h_2); % Swirl angle [deg]
 Vru_h_2 = Vu_h_2 - U_h; % Relative swirl velocity [m/s]
 Vr_h_2 = sqrt(Vru_h_2^2 + Va_h_2^2); % Relative velocity [m/s]
 alpha_r_h_2 = atand(Vru_h_2/Va_h_2); % Relative swirl angle [m/s]
-
+T_h_2 = To_2 - V_h_2^2/(2*Cp);
+M_h_2 = V_h_2 / sqrt(gamma*Rg*T_h_2);
+Tor_h_2 = T_h_2 + Vr_h_2^2/(2*Cp);
+Mr_h_2 = Vr_h_2/sqrt(gamma*Rg*T_h_2);
 
 % Station 3
 Va_h_3 = Va_3;
@@ -234,6 +234,10 @@ alpha_h_3 = atand(Vu_h_3/Va_h_3); % Swirl angle [deg]
 Vru_h_3 = U_h + Vu_h_3; % Relative swirl velocity [m/s]
 Vr_h_3 = sqrt(Va_h_3^2 + Vru_h_3^2); % Relative velocity [m/s]
 alpha_r_h_3 = atand(Vru_h_3/Va_h_3); % Relative swirl angle [m/s]
+T_h_3 = To_3 - V_h_3^2/(2*Cp);
+M_h_3 = V_h_3 / sqrt(gamma*Rg*T_h_3);
+Tor_h_3 = T_h_3 + Vr_h_3^2/(2*Cp);
+Mr_h_3 = Vr_h_3/sqrt(gamma*Rg*T_h_3);
 
 R_hub = (Vr_h_3^2 - Vr_h_2^2)/(Vr_h_3^2 - Vr_h_2^2 + V_h_2^2 - V_h_3^2);
 
@@ -244,7 +248,7 @@ end
 
 if U_h > max_U_h
     error = 1;
-    fprintf('fuck velocities\n')    
+    fprintf('fuck velocities\n')
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% TIP VELOCITY TRIANGLES %%%
@@ -257,6 +261,8 @@ Vu_t_1 = (r_m_1/r_t_1) * Vu_1; % tip swirl vel [m/s]
 Va_t_1 = Va_1; % tip axial vel [m/s]
 V_t_1 = sqrt(Va_t_1^2 + Vu_t_1^2); % tip velocity [m/s]
 alpha_t_1 = atand(Vu_t_1/Va_t_1); % Swirl angle [deg]
+T_t_1 = To_1 - V_t_1^2/(2*Cp);
+M_t_1 = V_t_1 / sqrt(gamma*Rg*T_t_1);
 
 % Station 2
 Va_t_2 = Va_2;
@@ -266,6 +272,10 @@ alpha_t_2 = atand(Vu_t_2/Va_t_2); % Swirl angle [deg]
 Vru_t_2 = Vu_t_2 - U_t; % Relative swirl velocity [m/s]
 Vr_t_2 = sqrt(Vru_t_2^2 + Va_t_2^2); % Relative velocity [m/s]
 alpha_r_t_2 = atand(Vru_t_2/Va_t_2); % Relative swirl angle [m/s]
+T_t_2 = To_2 - V_t_2^2/(2*Cp);
+M_t_2 = V_t_2 / sqrt(gamma*Rg*T_t_2);
+Tor_t_2 = T_t_2 + Vr_t_2^2/(2*Cp);
+Mr_t_2 = Vr_t_2/sqrt(gamma*Rg*T_t_2);
 
 
 % Station 3
@@ -276,6 +286,10 @@ alpha_t_3 = atand(Vu_t_3/Va_t_3); % Swirl angle [deg]
 Vru_t_3 = U_t + Vu_t_3; % Relative swirl velocity [m/s]
 Vr_t_3 = sqrt(Va_t_3^2 + Vru_t_3^2); % Relative velocity [m/s]
 alpha_r_t_3 = atand(Vru_t_3/Va_t_3); % Relative swirl angle [m/s]
+T_t_3 = To_3 - V_t_3^2/(2*Cp);
+M_t_3 = V_t_3 / sqrt(gamma*Rg*T_t_3);
+Tor_t_3 = T_h_3 + Vr_t_3^2/(2*Cp);
+Mr_t_3 = Vr_t_3/sqrt(gamma*Rg*T_t_3);
 
 R_tip = (Vr_t_3^2 - Vr_t_2^2)/(Vr_t_3^2 - Vr_t_2^2 + V_t_2^2 - V_t_3^2); % Reaction at tip
 
@@ -293,7 +307,7 @@ blade_actual_chord = blade_height/AR_b; % Blade actual chord [m]
 %%% STAGGER ANGLE %%%
 %%%%%%%%%%%%%%%%%%%%%
 
-stagger_data = csvread('stagger_angle.csv',1); % Stagger angle data 
+stagger_data = csvread('stagger_angle.csv',1); % Stagger angle data
 beta_1 = stagger_data(:,1); % beta before vane or blade
 beta_2 = stagger_data(:,2:end); % beta after vane or blade
 beta_2_vector = [80,75,70,65,60,55,50]; % set of curves that define the plot
@@ -314,6 +328,9 @@ vane_number = pi*(r_m_2+r_m_1)/vane_pitch; % Number of vanes
 blade_pitch = (zweif_blade*blade_axial_chord*0.5)/((tand(alpha_r_2) + tand(alpha_r_3))* ((cosd(alpha_r_3))^2)); % Blade pitch [m]
 blade_number = 2*pi*r_m_2/blade_pitch; % Number of blades
 
+fprintf('%3.2f vanes \n',vane_number);
+fprintf('%3.2f blades \n',blade_number);
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% THICKNESS OVER CHORD %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -323,11 +340,45 @@ tmaxc_data = csvread('tmaxc_v_betas.csv',1);
 vane_max_thickness = vane_actual_chord*interp1(tmaxc_data(:,1),tmaxc_data(:,2),alpha_1 + inc_1 + alpha_2 + dev_2);
 blade_max_thickness = blade_actual_chord*interp1(tmaxc_data(:,1),tmaxc_data(:,2),alpha_r_2 + inc_2 + alpha_r_3 + dev_3);
 
+%%%%%%%%%%%%%%
+%%% LOSSES %%%
+%%%%%%%%%%%%%%
 
-%%%%%%%%%%%%%%%%%%%
-%%% VANE LOSSES %%%
-%%%%%%%%%%%%%%%%%%%
+% k_Accel
 
+if M_2 <= 0.2
+    K1_vane = 1;
+else
+    K1_vane = 1-1.25*(M_2-0.2);
+end
+K2_vane = (M_1/M_2)^2;
+k_accel_vane = 1-K2_vane *(1- K1_vane);
+
+if M_r_3<=0.2
+    K1_blade = 1;
+else
+    K1_blade = 1-1.25*(M_r_3-0.2);
+end
+K2_blade = (M_r_2/M_r_3)^2;
+k_accel_blade = 1-K2_blade*(1-K1_blade);
+
+
+% Ksh, will always be equal to zero for vane though since M1 is 0.125
+if M_h_1 <= 0.4
+    P_Q_sh_vane=0;
+else
+    P_Q_sh_vane = (r_h_1/(0.5*(r_t_1+r_t_2)))*(0.75*(M_h_1-0.4)^1.75);
+end
+Ksh_vane = P_Q_sh_vane*(P_1/P_2)*(1-(1+0.5*(gamma-1)*M_1^2)^(gamma/(gamma-1)))/(1-(1+0.5*(gamma-1)*M_2^2)^(gamma/(gamma-1)));
+
+if Mr_h_2 <= 0.4
+     P_Q_sh_blade=0;
+else
+    P_Q_sh_blade = (r_h_3/r_t_3)*(0.75*(Mr_h_2-0.4)^1.75);
+end
+Ksh_blade = P_Q_sh_blade*(P_2/P_3)*(1-(1+0.5*(gamma-1)*M_r_2^2)^(gamma/(gamma-1)))/(1-(1+0.5*(gamma-1)*M_r_3^2)^(gamma/(gamma-1)));
+
+    
 
 
 
